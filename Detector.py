cf32445d4104096f1609c10528aab18aa66569f6
jaweria332 <hiyamughal03@gmail.com>
2021-05-09 17:42:41 +0500

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -53,3 +53,22 @@ data=np.array(data,dtype='float32')
 labels=np.array(labels)
 
 X_train,X_test,Y_train,Y_test=train_test_split(data,labels,test_size=0.20,statify=labels,random_state=42)
+
+
+#Construct Image data genertaor - create many image from single image
+aug=ImageDataGenerator(rotation_range=20,zoom_range=0.15,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.15,horizontal_flip=True,fill_mode="nearest")
+
+#Laoding MobileNetV2 network, ensuring head FC layer set
+baseModel = MobileNetV2(weights="imagenet",include_top=False,input_tensor=Input(shape=(224,224,3)))
+
+#Construct the head of model that will be place on te top of the base model
+headModel=baseModel.output
+headModel=AveragePooling2D(pool_size=(7,7))(headModel)
+headModel=Flatten(name="flatten")(headModel)
+headModel=Dense(128,activation="relu")(headModel)
+headModel=Dropout(0.5)(headModel)
+headModel=Dense(2,activation="softmax")(headModel)
+
+#Place head model on the top of base model
+model=Model(inputs=baseModel.input,outputs=headModel)
+
