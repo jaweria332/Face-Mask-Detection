10d8ca8f87ac46096449b2823c2acafd99e28925
jaweria332 <hiyamughal03@gmail.com>
2021-05-09 20:24:06 +0500

    Update VideoTester.py

--- VideoTester.py
+++ VideoTester.py
@@ -57,6 +57,7 @@ def detect_and_predict(frame,face,mask):
 #Load serialized face detector model
 prototxtpath=r"face_detector\deploy.prototxt"
 weightspath=r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+# DNN = Deep Neural Network - a new method of OpenCV
 faceNet=cv2.dnn.readNet(prototxtpath,weightspath)
 
 #Load face mask detector model
@@ -64,9 +65,41 @@ model=load_model("Detector.model")
 
 #Initializing the video stream
 print("Starting the video stream...")
-cap = cv2.VideoCapture(0)
+cap = VideoStream(src=0).start()
 
 while True:
-    frame=cv2.read()
+    frame=cap.read()
     frame=imutils.resize(frame,width=400)
-    
+
+    #Detect faces and predict whether wearing mask or not
+    (locs,preds)=detect_and_predict(frame,faceNet,model)
+
+    #Loop over detected face
+    for (box,pred) in zip(locs,preds):
+        #Unpack bounding box and predictions
+        (X_str,Y_str,X_end,Y_end) = box
+        (mask, withoutMask) = pred
+
+        #Determing different colors for each case of detection
+        label ="Mask" if mask>withoutMask else "No Mask"
+        color=(0,255,0) if label=="Mask" else (0,0,255)
+
+        #Including the probability on the label
+        label="{}: {:.2f}%".format(label,max(mask,withoutMask)*100)
+
+        #Display the label on the bounding box
+        cv2.putText(frame,label,(X_str,Y_str-10),cv2.FONT_HERSHEY_COMPLEX,0.45,color,2)
+        cv2.rectangle(frame,(X_str,Y_str),(X_end,Y_end),color,2)
+
+    #Showing output frame
+    cv2.imshow("Face Mask Detection",frame)
+    key=cv2.waitKey(1)  & 0xFF
+
+    #If 'q' key was pressed,break from the loop
+
+    if key == ord("q"):
+            break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+cap.stop()
